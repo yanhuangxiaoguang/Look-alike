a9adaa99eda5b6a9c86d23f6168b4a0cd30e4dd5
Candela <1012559000@qq.com>
2018-04-23 11:21:57 +0800

    Create hanmming.py

---
+++ hanmming.py
@@ -0,0 +1,5 @@
+def hammingDistance(s1, s2):
+    """Return the Hamming distance between equal-length sequences"""
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+    return sum(el1 != el2 for el1, el2 in zip(s1, s2))
